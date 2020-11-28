d38ee912848d4b8361af3147eaa0d1cbda2be5b1
dianalina99 <diana.dobrescu100@gmail.com>
2020-11-28 16:25:28 +0000

    Add player movements and animation

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -9,7 +9,8 @@ public class PlayerMovement : MonoBehaviour
     float horizontalMove = 0f;
     float verticalMove = 0f;
     private float runSpeed = 40f;
-    private bool jump = false;
+    private bool jump = false, crouch = false;
+    public Animator animator;
 
     // Start is called before the first frame update
     void Start()
@@ -22,14 +23,38 @@ public class PlayerMovement : MonoBehaviour
     {
         horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
 
+        animator.SetFloat("speed", Mathf.Abs(horizontalMove));
+
         if(Input.GetButton("Jump"))
         {
             jump = true;
+            animator.SetBool("jump", true);
+        }
+        if (Input.GetButtonDown("Crouch"))
+        {
+            crouch = true;
+            animator.SetBool("crouch", true);
+        }
+        else if (Input.GetButtonUp("Crouch"))
+        {
+            crouch = false;
+            animator.SetBool("crouch", false);
         }
 
         verticalMove = Input.GetAxisRaw("Vertical");
 
     }
+
+    public void OnLanding()
+    {
+        animator.SetBool("jump", false);
+    }
+
+    /*
+    public void OnCrouching(bool crouch)
+    {
+        animator.SetBool("crouch", crouch);
+    }*/
 
     private void FixedUpdate()
     {
