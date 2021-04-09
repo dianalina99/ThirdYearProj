41a5460767cee5549ea42343613a37ead8987123
dianalina99 <diana.dobrescu100@gmail.com>
2021-04-09 17:48:37 +0100

    Fix camera problem with going over edges

--- Assets/Scripts/Cave/FollowPlayer.cs
+++ Assets/Scripts/Cave/FollowPlayer.cs
@@ -11,7 +11,37 @@ public class FollowPlayer : MonoBehaviour
         //Check if player is spawned.
         if(GameManagerScript.instance.playerRef != null)
         {
-            this.transform.position = new Vector3(GameManagerScript.instance.playerRef.transform.position.x, GameManagerScript.instance.playerRef.transform.position.y, GameManagerScript.instance.playerRef.transform.position.z -5);
+            Vector3 newPos = new Vector3(GameManagerScript.instance.playerRef.transform.position.x, GameManagerScript.instance.playerRef.transform.position.y, GameManagerScript.instance.playerRef.transform.position.z -5);
+        
+            //Check if newpos is within forest bounds. - this is to avoid the camera moving over the edge of the map while player is still on the map.
+            if(GameManagerScript.instance.forestInUse)
+            {
+                //Up
+                if(newPos.y > 81.0371)
+                {
+                    newPos = new Vector3(newPos.x, 81.0371f, newPos.z);
+                }
+
+                //Right
+                if(newPos.x > 243.302f)
+                {
+                    newPos = new Vector3(243.302f, newPos.y, newPos.z);
+                }
+
+                //Left
+                if(newPos.x < 158.397f)
+                {
+                    newPos = new Vector3(158.397f, newPos.y, newPos.z);
+                }
+
+                //Down
+                if(newPos.y < -7.2999f)
+                {
+                    newPos = new Vector3(newPos.x, -7.2999f, newPos.z);
+                }
+            }
+
+            this.transform.position = newPos;
         }
 
         
